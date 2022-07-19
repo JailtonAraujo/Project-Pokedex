16577dc7b070d63886ea509c4743d83b25814304
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-07-19 00:20:31 -0300

    Implementando pagina de login

--- Front-End-Pokedex/src/app/app-routing.module.ts
+++ Front-End-Pokedex/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pokemon/home/home.component';
 import { MyPokedexComponent } from './components/pokemon/my-pokedex/my-pokedex.component';
 import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'about', component:AboutComponent},
-  {path:'mypokedex', component: MyPokedexComponent}
+  {path:'mypokedex', component: MyPokedexComponent},
+  {path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
